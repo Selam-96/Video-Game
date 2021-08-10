5c1ed2853e6adfc129dfc28ee4a27f666102934f
KD <Kidanemeng7@gmail.com>
2021-08-10 03:01:14 -0700

    update index.php

--- index.php
+++ index.php
@@ -54,14 +54,41 @@ function bondJSON(data){
 // Here is how i see data returned via Console
 	console.log(data);
 
+	//indetifies the type of data returned
+	$('#filmtitle').html(data.title);	
+
 //This loads the data on the page, but it is all bunched
 //$("#output").text(JSON.stringify(data));
 
 //this creates a map of JSON on our page
+/*
 let myData = JSON.stringify(data,null,4);
 myData = "<pre>" + myData + "</pre>";
 $("#output").html(myData);
 }
+*/
+
+
+function bondTemplate(film){
+
+	return `
+		<div class="film">
+			<b>Film:</b>${film.Film}<br />
+			<b>Title:</b>${film.Title}<br />
+			<b>Year:</b>${film.Year}<br />
+			<b>Director:</b>${film.Director}<br />
+			<b>Producers:</b>${film.Producers}<br />
+			<b>Writers:</b>${film.Writers}<br />
+			<b>Composer:</b>${film.Composer}<br />
+			<b>Bond:</b>Sean ${film.Bond}<br />
+			<b>Budget:</b>${film.Budget}<br />
+			<b>Box Office:</b>${film.BoxOffice}<br />	
+		<div class="pic"><img src="thumbnails/${film.Image}" /></div>
+	`;
+
+}
+
+
 
 </script>
 </head>
@@ -72,16 +99,16 @@ $("#output").html(myData);
 		<h3 id="filmtitle">Title Will Go Here</h3>
 		<div id="films">
 			<div class="film">
-			<b>Film:</b>1<br />
-			<b>Title:</b>Dr. No<br />
-			<b>Year:</b>1962<br />
-			<b>Director:</b>Terence Young<br />
-			<b>Producers:</b>Harry Saltzman and Albert R. Broccoli<br />
-			<b>Writers:</b>Richard Maibaum, Johanna Harwood and Berkely Mather<br />
-			<b>Composer:</b>Monty Norman<br />
-			<b>Bond:</b>Sean Connery<br />
-			<b>Budget:</b>$1,000,000.00<br />
-			<b>BoxOffice:</b>$59,567,035.00<br />	
+				<b>Film:</b>1<br />
+				<b>Title:</b>Dr. No<br />
+				<b>Year:</b>1962<br />
+				<b>Director:</b>Terence Young<br />
+				<b>Producers:</b>Harry Saltzman and Albert R. Broccoli<br />
+				<b>Writers:</b>Richard Maibaum, Johanna Harwood and Berkely Mather<br />
+				<b>Composer:</b>Monty Norman<br />
+				<b>Bond:</b>Sean Connery<br />
+				<b>Budget:</b>$1,000,000.00<br />
+				<b>Box Office:</b>$59,567,035.00<br />	
 			<div class="pic"><img src="thumbnails/dr-no.jpg" /></div>
 		</div>
 		<div id="output">Results go here</div>
