94a234170ef6636f535c162b8731fb8daafe1b18
KD <Kidanemeng7@gmail.com>
2021-08-10 03:15:58 -0700

    update index.php

--- index.php
+++ index.php
@@ -54,8 +54,14 @@ function bondJSON(data){
 // Here is how i see data returned via Console
 	console.log(data);
 
-	//indetifies the type of data returned
-	$('#filmtitle').html(data.title);	
+	//indentifies the type of data returned
+	$('#filmtitle').html(data.title);
+
+	$.each(data.films.funtion(i,item){
+		let myFilm = bondTemplate(item)
+
+		$('<div></div>').html(myFilm).appendTo('#films');
+	});	
 
 //This loads the data on the page, but it is all bunched
 //$("#output").text(JSON.stringify(data));
@@ -98,6 +104,7 @@ function bondTemplate(film){
 		<a href="box" class="category">Bond Films By International Box Office Totals</a>
 		<h3 id="filmtitle">Title Will Go Here</h3>
 		<div id="films">
+		<!--
 			<div class="film">
 				<b>Film:</b>1<br />
 				<b>Title:</b>Dr. No<br />
@@ -109,7 +116,9 @@ function bondTemplate(film){
 				<b>Bond:</b>Sean Connery<br />
 				<b>Budget:</b>$1,000,000.00<br />
 				<b>Box Office:</b>$59,567,035.00<br />	
-			<div class="pic"><img src="thumbnails/dr-no.jpg" /></div>
+				<div class="pic"><img src="thumbnails/dr-no.jpg" /></div>
+			</div>
+			-->
 		</div>
 		<div id="output">Results go here</div>
 	</body>
