e71b1d4180ec7ccd08d0f8950def637a8f5e6e65
KD <Kidanemeng7@gmail.com>
2021-08-14 06:30:33 -0700

    Adds games-year.js

---
+++ data/games-year.js
@@ -0,0 +1,52 @@
+{
+	"title": "Video Games by Year",
+	"games":[
+			  {
+				"Title":"Sonic the Hedgehog",
+				"Genre":"Platformer",
+				"Company":"Sega",
+				"Year":1991,
+				"Rating":"E",
+				"Image":"sonic-the-hedgehog.jpg"
+			  },
+			  {
+				"Title":"Mario Cart",
+				"Genre":"Racing",
+				"Company":"Nintendo",
+				"Year":1992,
+				"Rating":"E",
+				"Image":"mario-cart.jpg"
+			  }, {
+				"Title":"Portal",
+				"Genre":"Puzzle",
+				"Company":"Valve",
+				"Year":2007,
+				"Rating":"T",
+				"Image":"portal.jpg"
+			  }, {
+				"Title":"Minecraft",
+				"Genre":"Sandbox",
+				"Company":"Mojang",
+				"Year":2011,
+				"Rating":"E",
+				"Image":"minecraft.jpg"
+			  },{
+				"Title":"Counter-Strike Global Offensive",
+				"Genre":"Tactical Shooter",
+				"Company":"Valve",
+				"Year":2012,
+				"Rating":"M",
+				"Image":"counter-strike-global-offensive.jpg"
+			  }
+	]
+}
+
+/*
+
+Sonic the Hedgehog, Platformer, Sega, 1991, E
+Mario Cart, Racing, Nintendo, 1992, E
+Portal, Puzzle Valve, 2007, T
+Minecraft, Sandbox, Mojang, 2011, E
+Counter-Strike Global Offensive, Tactical Shooter, Valve, 2012, M
+
+*/
